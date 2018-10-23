a9bc2151c1b78762fdd0c5a4c6032991cbe7f164
Kabra <skabra@cci14lj1gqaws.dyn.uncc.edu>
2018-10-23 19:03:26 -0400

    first commit

---
+++ InClass02a/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  InClass02a
+//
+//  Created by Kabra, Sunidhi on 10/23/18.
+//  Copyright © 2018 Kabra, Sunidhi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var firstNumberEditText: UITextField!
+    @IBOutlet weak var secondNumberEdittext: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
